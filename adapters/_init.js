1b5c33093d0e7ca245e79ed3d1fc751d35e581ed
Julien Vanier <jvanier@gmail.com>
2019-11-11 12:06:23 -0500

    Update mongodb env var

--- adapters/_init.js
+++ adapters/_init.js
@@ -2,5 +2,5 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-var url = process.env.MONGODB_URL || 'mongodb://localhost/slack_time';
+var url = process.env.MONGO_URL || 'mongodb://localhost/slack_time';
 mongoose.connect(url);
